4c65b623b361c7d8a41ec0abd1cb065ee3d511ba
Teerakarn Fongkham <118352870+FirstTeerakarn@users.noreply.github.com>
2025-03-22 02:30:47 +0700

    Add files via upload

---
+++ To-do list calendar/lib/models/task_model.dart
@@ -0,0 +1,17 @@
+import 'package:hive/hive.dart';
+
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  bool isCompleted;
+
+  @HiveField(2)
+  DateTime? date;
+
+  Task({required this.title, this.isCompleted = false, this.date});
+}
